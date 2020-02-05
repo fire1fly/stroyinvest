aa9b8e393123d8ef3d3d3ceb35bc9140db915f5b
Danil <56827297+fire1fly@users.noreply.github.com>
2020-02-05 12:44:45 +0800

    new pages, calc, mobile ver

--- js/script.js
+++ js/script.js
@@ -1,5 +1,65 @@
 $(document).ready(() => {
 
+  let perc = 0.2, month = 6, result, total,
+  monthly = 1666,
+  money = 100000;
+
+
+  $('.calc-programms').on('click', (event) => {
+    let target = event.target;
+    if(target.id === 'first'){
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } else if (target.id === 'second') {
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } else if (target.id === 'third') {
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } else if (target.id === 'fourth') {
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } else if (target.id === 'fifth') {
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    }
+    
+    result = Math.round(perc / 12 * month * money);
+    total = result + money;
+    monthly = parseInt(result / month);
+    $('.calc-total span').text(total.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    $('.calc-total small span').text(month);
+    $('.calc-monthly span').text(monthly.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+  });
+  
+  $('.calc-range').on('change', (event) => {
+    $('.calc-summ__invest-num span').text($('.calc-range').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    let target = event.target;
+    money = +$('.calc-range').val();
+    if(target.id === 'first'){
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } else if (target.id === 'second') {
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } else if (target.id === 'third') {
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } else if (target.id === 'fourth') {
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } else if (target.id === 'fifth') {
+      month = +target.getAttribute('data-month');
+      perc = +target.getAttribute('data-perc');
+    } 
+    result = Math.round(perc / 12 * month * money);
+    total = result + money;
+    monthly = parseInt(result / month);
+    +$('.calc-total span').text(total.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    $('.calc-total small span').text(month);
+    +$('.calc-monthly span').text(monthly.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+  });
+
   // Настройка slick slider
   $('.directions-blocks').slick({
     arrows: false,
@@ -37,26 +97,6 @@ $(document).ready(() => {
   $('.left-sidebar__btn').on('click', (event) => {
     event.preventDefault();
     $('.left-sidebar__menu').slideToggle(400);
-  });
-
-  let elem = document.querySelector('.calc-range');
-  let init = new Powerange(elem, { min: 100000, 
-                                  max: 3000000, 
-                                  start: 100000, 
-                                  hideRange: true,
-                                  step: 100000 });
-  let radio = $('input[name="programms"]'),
-      perc = 0.2,
-      month = 6,
-      money = $('.calc-range').val(),
-      result;
-  
-  $('.calc-range').on('change', (event) => {
-    $('.calc-summ__invest-num span').text($('.calc-range').val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '))
-  });
-
-  radio.on('change', () => {
-    console.log(radio.attr('id'));
   });
 
 });
